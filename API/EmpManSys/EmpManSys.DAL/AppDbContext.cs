79127664dc18a48d7fb4440abde03f9d1abea982
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-09-28 00:14:33 +0300

    first commit API + AppDbContext

---
+++ API/EmpManSys/EmpManSys.DAL/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmpManSys.DAL;
+
+public class AppDbContext : DbContext
+{
+    private readonly string? _connectionString;
+
+
+    public AppDbContext(string? connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString))
+            throw new ArgumentNullException(nameof(connectionString));
+
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder opBuilder)
+    {
+        opBuilder.UseSqlServer(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ API/EmpManSys/EmpManSys.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmpManSys.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    [Route("Index")]
+    public string Index()
+    {
+        return "Hello World!";
+    }
+}
